7109080e7e1a0fe67467d2b7f8151dbe9e2e6066
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2026-06-01 15:18:57 -0300

    Add files via upload

---
+++ exercicio_realloc_free.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int *numeros = NULL, qtd_numeros = 0;
+	
+	while (1) {
+		int numero;
+		
+		scanf("%d", &numero);
+		
+		if (!numero) break;
+		else {
+			numeros = (int *)realloc(numeros, ++qtd_numeros * sizeof(int));
+			
+			if (!numeros) exit(1);
+			
+			*(numeros + (qtd_numeros - 1)) = numero;
+		}
+	}
+	
+	int i;
+	for (i = 0; i < qtd_numeros; i++) {
+		printf("%d ", *(numeros + i));
+	}
+	
+	free(numeros);
+	
+	return 0;
+}
